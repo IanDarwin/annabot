19b4b99bbca4e5fb978acbb12e86726ed4b19acf
IanDarwin <>
2009-01-05 20:13:28 +0000

    First toy version of AnnaBot

---
+++ src/annabot/AnnaBot0.java
@@ -0,0 +1,44 @@
+package annabot;
+
+import demo.MyAnnotation;
+
+import java.lang.reflect.*;
+import java.lang.annotation.*;
+
+/*
+ * Very simple toy version, just to show checking annotations code
+ */
+public class AnnaBot1 {
+
+	@MyAnnotation
+	int i;
+
+	@MyAnnotation
+	public static void main(String[] args) throws Exception {
+		if (args.length == 0) {
+			System.out.println( "Usage: AnnaBot1 className [...]");
+			return;
+		}
+		String clName = args[0];
+		Class c = Class.forName(clName);
+		// Don't use getDeclaredXXX() here, we do care about
+		// inherited methods/fields that are annotated.
+		Field[] fields = c.getFields();
+		Method[] methods = c.getMethods();
+		boolean fieldHasJpaAnno = false, methodHasJpaAnno = false;
+		for (Field field : fields) {
+			System.out.println(field);
+			Annotation[] ann = field.getDeclaredAnnotations();
+			for (Annotation a : ann) {
+				System.out.println(field.getName() + "--" + a);
+			}
+		}
+		for (Method method : methods) {
+			System.out.println(method);
+			Annotation[] ann = method.getDeclaredAnnotations();
+			for (Annotation a : ann) {
+				System.out.println(method.getName() + "--" + a);
+			}
+		}
+	}
+}
---
+++ src/demo/MyAnnotation.java
@@ -0,0 +1,15 @@
+import java.lang.annotation.Annotation;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * A sample annotation for types (classes, interfaces);
+ * it will be available at run time.
+ */
+@Target({ElementType.TYPE,ElementType.METHOD,ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MyAnnotation {
+	public boolean useless() default false;
+}
