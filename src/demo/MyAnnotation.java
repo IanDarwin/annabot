21ddc6479da8ed46da75f50e56d812e3ae022244
IanDarwin <>
2009-01-05 21:58:14 +0000

    Add missing package statement.

--- src/demo/MyAnnotation.java
+++ src/demo/MyAnnotation.java
@@ -1,4 +1,5 @@
-import java.lang.annotation.Annotation;
+package demo;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
