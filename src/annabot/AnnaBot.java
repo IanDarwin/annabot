3e6e6b3d4e65a5602eae7df5717f6d7498069183
IanDarwin <>
2009-01-21 13:10:35 +0000

    Comment.

--- src/annabot/AnnaBot.java
+++ src/annabot/AnnaBot.java
@@ -3,6 +3,7 @@ package annabot;
 public class AnnaBot2 {
 
 	/**
+	 * Annotations Assertion Based Object Testing for Java
 	 * A version in which the user provides a pre-compiled
 	 * parse list and a package name. The DSL is effectively
 	 * a set of Constructor calls.
