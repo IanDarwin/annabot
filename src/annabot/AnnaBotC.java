4aae4d33282241f5cf96ffd1cb0c8628b59775d7
IanDarwin <>
2009-01-21 12:59:34 +0000

    Process args; if debug, send tree to graphviz.

--- src/annabot/AnnaBotC.java
+++ src/annabot/AnnaBotC.java
@@ -1,9 +1,14 @@
 package annabot;
 
+import java.io.FileInputStream;
+import java.io.PrintStream;
+
 import org.antlr.runtime.ANTLRInputStream;
 import org.antlr.runtime.CommonTokenStream;
 import org.antlr.runtime.tree.CommonTree;
 import org.antlr.runtime.tree.CommonTreeNodeStream;
+import org.antlr.runtime.tree.DOTTreeGenerator;
+import org.antlr.stringtemplate.StringTemplate;
 
 import annabot.antlr.GrammarLexer;
 import annabot.antlr.GrammarParser;
@@ -11,17 +16,31 @@ import annabot.antlr.TreeWalker;
 
 public class AnnaBot {
 	
-    public static void main(String[] args) throws Exception {
+    private static boolean debug = true;
+
+	public static void main(String[] args) throws Exception {
         // Build the tree.
-    	ANTLRInputStream input = new ANTLRInputStream(System.in);
+    	ANTLRInputStream input;
+    	if (args.length == 0)
+			input = new ANTLRInputStream(System.in);
+		else
+			input = new ANTLRInputStream(new FileInputStream(args[0]));
         GrammarLexer lexer = new GrammarLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         GrammarParser parser = new GrammarParser(tokens);
         GrammarParser.program_return r = parser.program();
 
-        // Walk resulting tree once, for now.
-        // XXX have to walk once per class being tested.
         CommonTree t = (CommonTree)r.getTree();
+        if (debug) {
+        	DOTTreeGenerator gen = new DOTTreeGenerator();
+        	StringTemplate st = gen.toDOT(t);
+        	Process p = Runtime.getRuntime().exec("dot -T xlib");
+        	new PrintStream(p.getOutputStream()).print(st);
+
+        }
+        
+        // Walk resulting tree once, for now.
+        // XXX have to walk once per class being tested.        
         CommonTreeNodeStream nodes = new CommonTreeNodeStream(t);
         TreeWalker walker = new TreeWalker(nodes);
         walker.program();
