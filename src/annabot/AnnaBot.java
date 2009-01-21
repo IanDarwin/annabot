106863a17d5573fbf294a7d2db1370dce224c823
IanDarwin <>
2009-01-21 01:36:31 +0000

    Initial revision

---
+++ src/annabot/AnnaBot.java
@@ -0,0 +1,14 @@
+package annabot;
+
+public class AnnaBot2 {
+
+	/**
+	 * A version in which the user provides a pre-compiled
+	 * parse list and a package name. The DSL is effectively
+	 * a set of Constructor calls.
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
