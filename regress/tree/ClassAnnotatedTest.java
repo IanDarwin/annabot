2fafec5724d05b0448173d63b356ce7b35587297
IanDarwin <>
2009-01-24 03:47:30 +0000

    ClassAnnotated is now written. And even tested. There will be tests.

---
+++ regress/tree/ClassAnnotatedTest.java
@@ -0,0 +1,39 @@
+package tree;
+
+import org.junit.Test;
+
+import demo.MyAnnotation;
+import static org.junit.Assert.*;
+
+@MyAnnotation
+public class ClassAnnotatedTest {
+
+	@Test public void testTrueWithStringClassName() {
+		Operator t = new ClassAnnotated("demo.MyAnnotation");
+		assertTrue(t.process(ClassAnnotatedTest.class));
+	}
+	@Test public void testFalseWithStringClassName() {
+		Operator t = new ClassAnnotated("foo.Goo");
+		assertFalse(t.process(ClassAnnotatedTest.class));
+	}
+
+	@Test public void testTrueWithStringPackageRef() {
+		Operator t = new ClassAnnotated("demo.*");
+		assertTrue(t.process(ClassAnnotatedTest.class));
+	}
+	
+	@Test public void testFalseWithStringPackageRef() {
+		Operator t = new ClassAnnotated("dummy.*");
+		assertFalse(t.process(ClassAnnotatedTest.class));
+	}
+
+	@Test public void testTrueWithAnnotationClass() {
+		Operator t = new ClassAnnotated(java.lang.SuppressWarnings.class);
+		assertTrue(t.process(ClassAnnotatedTest.class));
+	}
+
+	@Test public void testFalseWithAnnotationClass() {
+		Operator t = new ClassAnnotated(java.lang.SuppressWarnings.class);
+		assertTrue(t.process(Object.class));
+	}
+}
--- src/tree/ClassAnnotated.java
+++ src/tree/ClassAnnotated.java
@@ -1,22 +1,47 @@
 package tree;
 
+import java.lang.annotation.Annotation;
+import java.util.regex.Pattern;
 
 /** Placeholder when the target that must be 
  * annotated is a Class.
  */
 public class ClassAnnotated implements Operator {
 	
+	private final String annotationName;
+	private final Pattern regex;
+	private final Class<? extends Annotation> annotationClass;
+
 	public ClassAnnotated(String name) {
-		
+		this.annotationName = name;
+		this.regex = Pattern.compile(name.replaceAll("\\*", ".*"));
+		this.annotationClass = null;
 	}
-	
-	/** The required annotation; sadly we cannot
-	 * use java.lang.annotation.Annotation as its type.
-	 */
-	Class<?> annotation;
+
+	public ClassAnnotated(Class<? extends Annotation> cl) {
+		this.annotationName = null;
+		this.regex = null;
+		this.annotationClass = cl;
+	}
 
 	public boolean process(Class<?> c) {
-		// TODO Auto-generated method stub
-		return false;
+		final String clazzName = c.getName();
+		if (annotationName != null) {
+			final Annotation[] classAnnotations = c.getDeclaredAnnotations();
+			for (Annotation annotation : classAnnotations) {
+				String string = annotation.toString();
+				String annName = string.substring(1).replaceFirst("\\(.*", "");
+				if (match(annName)) {
+					return true;
+				}				
+			}
+			return false;
+		} else {
+			return null == c.getAnnotation(annotationClass);
+		}
+	}
+
+	private boolean match(String annName) {
+		return regex.matcher(annName).matches();
 	}
 }
