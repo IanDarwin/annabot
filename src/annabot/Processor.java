f83988eff207b510d38978d4eeca1dd14d5359f9
IanDarwin <>
2009-01-25 20:40:20 +0000

    ClassFilter allowed to be null.

--- src/annabot/Processor.java
+++ src/annabot/Processor.java
@@ -3,25 +3,37 @@ package annabot;
 import tree.Operator;
 
 public class Processor {
-	Operator[] checks;
+	Claim checks;
 	
 	public Processor() {
 		// empty
 	}
 	
-	public Processor(Operator[] checks) {
+	public Processor(Claim checks) {
 		super();
 		this.checks = checks;
 	}
 	
 	/** Process a given class with  the default list of Operators */
-	public boolean process(Class<?> target, Operator[] checks) {
+	public boolean process(Class<?> target, Claim checks) {
 		if (checks == null) {
 			throw new IllegalStateException(
 			"Must provide Operator[] to constructor or to process() method"
 			);
 		}
-		for (Operator check: checks) {
+		
+		// If the Claim does not apply to this class,
+		// it is not an error, so return true.
+		final Operator[] classFilter = checks.getClassFilter();
+		if (classFilter != null) {
+			for (Operator check: classFilter) {
+				if (!check.process(target)) {
+					return true;
+				}
+			}
+		}
+		// Run the actual tests
+		for (Operator check: checks.getOperators()) {
 			if (!check.process(target))
 				return false;
 		}
