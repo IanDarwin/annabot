b97ef177f9913b48e267ef3ccf44b753d3c2de69
IanDarwin <>
2009-01-25 19:36:44 +0000

    Get serious.

--- regress/annabot/ProcessTest.java
+++ regress/annabot/ProcessTest.java
@@ -1,5 +1,6 @@
 package annabot;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -12,14 +13,38 @@ import demo.MyAnnotation;
 
 
 public class ProcessTest {
+	
+	Operator[] checks = {
+		new RequiresOne(
+			new MethodAnnotated(MyAnnotation.class))
+	};
+	Operator[] failingChecks = {
+		new RequiresOne(
+			new MethodAnnotated(MyAnnotation.class))
+	};
+	
 	@Test
-	public void main() {
-		Operator[] checks = {
-			new RequiresOne(
-				new MethodAnnotated(MyAnnotation.class))
-		};
+	public void testOneArgConstructor() {
 		Processor p = new Processor(checks);
 		assertTrue(
-				p.process(MyAnnotatedClass.class));
+			p.process(MyAnnotatedClass.class));
+	}
+	
+	@Test
+	public void testFailingOneArgConstructor() {
+		Processor p = new Processor(failingChecks);
+		assertFalse(
+			p.process(Object.class));
+	}
+	@Test
+	public void testNoArgConstructor() {
+		Processor p = new Processor();
+		assertTrue(
+			p.process(MyAnnotatedClass.class, checks));
+	}
+	@Test(expected=IllegalStateException.class)
+	public void testFailureNoArgConstructor() {
+		Processor p = new Processor();
+		p.process(MyAnnotatedClass.class);
 	}
 }
