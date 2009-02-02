b11b237a1d2a29f5fa079fc885df1446037e2bcb
IanDarwin <>
2009-02-02 17:49:03 +0000

    Add conflicting-EJB claim.

---
+++ javaclaims/ejb3/EJB3TypeClaim.java
@@ -0,0 +1,34 @@
+package ejb3;
+
+import javax.ejb.Stateful;
+import javax.ejb.Stateless;
+import javax.persistence.Entity;
+
+import tree.AtMostOne;
+import tree.ClassAnnotated;
+import tree.Operator;
+import annabot.Claim;
+
+public class EJB3TypeClaim extends Claim {
+
+	@Override
+	public Operator[] getClassFilter() {
+		// no filter needed, it's light-weight
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		return "Class has conflicting top-level EJB annotations";
+	}
+
+	@Override
+	public Operator[] getOperators() {
+		return new Operator[] {
+		    new AtMostOne(
+		    		new ClassAnnotated(Entity.class), 
+		    		new ClassAnnotated(Stateless.class), 
+		    		new ClassAnnotated(Stateful.class))
+		};
+	}
+}
