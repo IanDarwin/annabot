c620258b8ad7e392901b1bc144bc74876c510099
IanDarwin <>
2009-02-17 02:09:23 +0000

    Major interface, comment it.

--- src/annabot/Claim.java
+++ src/annabot/Claim.java
@@ -7,7 +7,25 @@ import tree.Operator;
  * about one API.
  */
 public abstract class Claim {
-	public abstract Operator[] getClassFilter();
-	public abstract Operator[] getOperators();
+	/**
+	 * @return The description of the problem this claim finds;
+	 * a better name might have been getErrorText().
+	 */
 	public abstract String getDescription();
+	
+	/**
+	 * @return A list of Operators that are the "if" part of
+	 * a claim; if any of these operators fails, the class
+	 * will be bypassed and considered successful for this claim.
+	 */
+	public abstract Operator[] getClassFilter();
+
+	/**
+	 * @return a loist of Operators, all of which must return
+	 * true for this class to be considered successful against
+	 * this claim.
+	 * Only used if the Operators in getClassFilter all pass.
+	 */
+	public abstract Operator[] getOperators();
+
 }
