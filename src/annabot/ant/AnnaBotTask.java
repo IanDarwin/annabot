29dbb8888e9ccb6eacb0da5e97547cfd1b918659
IanDarwin <>
2009-05-11 11:58:09 +0000

    Ant task now captures one set of inputs from <fileset> for claims and targets.

--- src/annabot/ant/AnnaBotTask.java
+++ src/annabot/ant/AnnaBotTask.java
@@ -4,27 +4,40 @@ import java.util.Iterator;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
-import org.apache.tools.ant.types.Path;
+import org.apache.tools.ant.types.FileSet;
+import org.apache.tools.ant.types.resources.FileResource;
 
 public class AnnaBotTask extends Task {
 
-	private Path claims;
+	private FileSet claims;
+	private FileSet targets;
 
 	@Override
 	public void init() throws BuildException {
 		super.init();
 	}
 	
-	public void setClaims(Path claims) {
+	public void addConfiguredClaims(FileSet claims) {
 		this.claims = claims;
+	}
+	
+	public void addConfiguredTargets(FileSet targets) {
+		this.targets = targets;
 	}
 	
 	@Override
 	public void execute() throws BuildException {
-		final Iterator claimList = claims.iterator();
+		System.out.println("CLAIMS:");
+		final Iterator<FileResource> claimList = claims.iterator();
 		while (claimList.hasNext()) {
-			Object o = claimList.next();
-			System.out.println(o.getClass());
+			FileResource o = claimList.next();
+			System.out.println(o);
+		}
+		
+		System.out.println("TARGETS");
+		final Iterator<FileResource> targetsList = targets.iterator();
+		while (targetsList.hasNext()) {
+			FileResource o = targetsList.next();
 			System.out.println(o);
 		}
 		super.execute();
