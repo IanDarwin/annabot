3269169f53ae626316f4b36a11e1b25c0676858d
IanDarwin <>
2009-05-12 16:20:43 +0000

    Split one claim into two. Small overhead and more declarative.

--- javaclaims/jpa/JPAEntityNotSetters.java
+++ javaclaims/jpa/JPAEntityNotSetters.java
@@ -1,25 +1,26 @@
 package jpa;
 
+import javax.persistence.Entity;
+
 import annabot.Claim;
 import tree.*;
 
-public class JPAEntityMethFieldClaim extends Claim {
+public class JPAEntityNotSetters extends Claim {
 	
 	public String getDescription() {
 		return "JPA Entities may have field OR method annotations, not both";
 	}
 	public Operator[] getClassFilter() {
 		return new Operator[] {
-			new ClassAnnotated("javax.persistence.Entity"),	
+			new ClassAnnotated(Entity.class),	
 		};
 	}
 
 	public Operator[] getOperators() {
 		return new Operator[] {
-			// make sure they don't annotate both methods and fields
-			new AtMostOne(
-					new FieldAnnotated("javax.persistence.*"),
-					new MethodAnnotated("javax.persistence.*")),
-			};
+			// make sure if they annotate methods, it's not setters
+			new None(
+					new MethodAnnotated("javax.persistence.*", "set*"))
+		};
 	}
 }
