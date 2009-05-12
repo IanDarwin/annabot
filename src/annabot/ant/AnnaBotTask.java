634a40bc6df6f23a70c1c095e19c14a7969a5cd9
IanDarwin <>
2009-05-12 10:46:05 +0000

    Work-in-progress: the Ant task is fully coded but doesn't work due to class names being instantiated without their package structure. Need to keep track of the starting directory. Build.xml has "systests" target which shows how to use the current version of the Ant task.

--- src/annabot/ant/AnnaBotTask.java
+++ src/annabot/ant/AnnaBotTask.java
@@ -1,16 +1,28 @@
 package annabot.ant;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
 import org.apache.tools.ant.types.FileSet;
+import org.apache.tools.ant.types.Path;
 import org.apache.tools.ant.types.resources.FileResource;
+
+import com.darwinsys.io.ClassSourceUtils;
+
+import annabot.AnnaBot;
 
 public class AnnaBotTask extends Task {
 
 	private FileSet claims;
 	private FileSet targets;
+	private Path inputClassPath;
 
 	@Override
 	public void init() throws BuildException {
@@ -25,21 +37,71 @@ public class AnnaBotTask extends Task {
 		this.targets = targets;
 	}
 	
+	public void addConfiguredClassPath(Path p) {
+		this.inputClassPath = p;
+		System.out.println(inputClassPath);
+	}
+	
 	@Override
 	public void execute() throws BuildException {
+		System.out.println("CLASSPATH");
+		List<URL> classpath = new ArrayList<URL>();
+		if (inputClassPath != null) {
+			Iterator<FileResource> it = inputClassPath.iterator();
+			while (it.hasNext()) {
+				FileResource o = it.next();
+				System.out.println(o + "--" + o.getClass());
+				try {
+					URL fileURL = ClassSourceUtils.makeFileURL(o.getFile().getAbsolutePath());
+					classpath.add(fileURL);
+				} catch (IOException e) {
+					throw new BuildException("getting url for classpath: " + e, e);
+				}
+			}
+		}
 		System.out.println("CLAIMS:");
+		List<Class<?>> claimClasses = new ArrayList<Class<?>>();
+		URL claimsURL = null;
+		try {
+			claimsURL = ClassSourceUtils.makeFileURL(claims.getDir().getAbsolutePath());
+		} catch (IOException e1) {
+			throw new BuildException("Make Class loader URL failed + e", e1);
+		}
+		List<URL> claimsURLs = new ArrayList<URL>(classpath);
+		claimsURLs.add(claimsURL);
+		ClassLoader cl = new URLClassLoader(claimsURLs.toArray(new URL[claimsURLs.size()]));
 		final Iterator<FileResource> claimList = claims.iterator();
 		while (claimList.hasNext()) {
 			FileResource o = claimList.next();
-			System.out.println(o);
+			File file = o.getFile();
+			claimClasses.add(ClassSourceUtils.doFile(file, cl));
+			System.out.println(file);
 		}
 		
 		System.out.println("TARGETS");
+		URL urlt = null;
+		try {
+			urlt = ClassSourceUtils.makeFileURL(claims.getDir().getAbsolutePath());
+		} catch (IOException e1) {
+			throw new BuildException("Make Class loader failed + e", e1);
+		}
+		List<URL> targetsURLs = new ArrayList<URL>(classpath);
+		targetsURLs.add(urlt);
+		ClassLoader clt = new URLClassLoader(targetsURLs.toArray(new URL[claimsURLs.size()]));
+		List<Class<?>> targetClasses = new ArrayList<Class<?>>(); // ClassSourceUtils.classListFromSource(classesToTest, classPathElements);
 		final Iterator<FileResource> targetsList = targets.iterator();
 		while (targetsList.hasNext()) {
 			FileResource o = targetsList.next();
-			System.out.println(o);
+			File file = o.getFile();
+			targetClasses.add(ClassSourceUtils.doFile(file, clt));
+			System.out.println(file);
 		}
-		super.execute();
+		
+		// The real work
+		try {
+			AnnaBot.process(targetClasses, claimClasses);
+		} catch (Exception e) {
+			throw new BuildException("AnnaBot threw exception: e", e);
+		}
 	}
 }
